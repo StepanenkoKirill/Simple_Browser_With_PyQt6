08c1613d3d3d89ea0d6fbbf86b2b4ca6ecedb737
StepanenkoKirill <123152402+StepanenkoKirill@users.noreply.github.com>
2023-09-12 21:53:58 +0300

    finished browser class

--- browser_class.py
+++ browser_class.py
@@ -13,3 +13,28 @@ class brouser(QWidget):
 
         self.address_str = QLineEdit(self)
         self.address_str.returnPressed.connect(self.loader)
+
+        self.prev_butt = QPushButton("Previous page", self)
+        self.prev_butt.clicked.connect(self.web_window.back)
+
+        self.next_butt = QPushButton("Next page", self)
+        self.next_butt.clicked.connect(self.web_window.forward)
+
+        self.search_butt = QPushButton("Search", self)
+        self.search_butt.clicked.connect(self.loader)
+
+        self.layout = QVBoxLayout(self)
+        self.button_layout = QHBoxLayout(self)
+
+        self.button_layout.addWidget(self.prev_butt)
+        self.button_layout.addWidget(self.next_butt)
+        self.button_layout.addWidget(self.search_butt)
+
+        self.layout.addLayout(self.button_layout)
+        self.layout.addWidget(self.web_window)
+
+    def loader(self):
+        url = self.address_str.text()
+        if(not url.startswith("http")):
+            url = "https://" + url
+        self.web_window.load(QUrl(url))
